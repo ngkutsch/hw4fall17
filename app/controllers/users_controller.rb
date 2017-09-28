2979d8d273ec96ea43f16b7e6af12a24001dc40e
Nickolas Kutsch <nickolas-kutsch@uiowa.edu>
2017-09-28 00:17:20 +0000

    fixing button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
     
     def user_params
-        params.require(:user).permit(:user_id, :email)
+        params.require(:user).permit(:user_id, :email, :session_token)
     end
     def new
+         # default: render 'new' template
     end
     
     def create 
         @user = User.create!(user_params)
         flash[:notice] = "#{@user.user_id} was successfully created."
-        redirect_to movies_path
+        redirect_to users_path
     end
 end
